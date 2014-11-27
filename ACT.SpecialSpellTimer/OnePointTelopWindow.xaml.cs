3bd85ae09686c225a22febb2eea64737c005bbd5
anoyetta <anoyetta@gmail.com>
2014-11-27 18:38:39 +0900

    整形

--- ACT.SpecialSpellTimer/OnePointTelopWindow.xaml.cs
+++ ACT.SpecialSpellTimer/OnePointTelopWindow.xaml.cs
@@ -6,6 +6,7 @@
     using System.Windows.Media;
     using System.Windows.Shapes;
     using System.Windows.Threading;
+
     using ACT.SpecialSpellTimer.Properties;
     using ACT.SpecialSpellTimer.Utility;
 
