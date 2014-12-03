7f97b66d6ae72bbd86ef26c2fe31199fbd270ba4
anoyetta <anoyetta@gmail.com>
2014-12-04 02:45:30 +0900

    整形

--- ACT.SpecialSpellTimer/SpellTimerListWindow.xaml.cs
+++ ACT.SpecialSpellTimer/SpellTimerListWindow.xaml.cs
@@ -5,12 +5,12 @@
     using System.Diagnostics;
     using System.Linq;
     using System.Runtime.InteropServices;
-    using System.Threading.Tasks;
     using System.Windows;
     using System.Windows.Controls;
     using System.Windows.Input;
     using System.Windows.Interop;
     using System.Windows.Media;
+
     using ACT.SpecialSpellTimer.Properties;
 
     /// <summary>
