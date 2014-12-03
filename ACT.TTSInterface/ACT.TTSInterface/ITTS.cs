a17c6816e6dbca6633c600ed4550f20cb27fa65b
anoyetta <anoyetta@gmail.com>
2014-12-03 10:39:59 +0900

    インターフェースもソース管理に追加した

---
+++ ACT.TTSInterface/ACT.TTSInterface/ITTS.cs
@@ -0,0 +1,15 @@
+﻿namespace ACT.TTSInterface
+{
+    /// <summary>
+    /// TTSインターフェイス
+    /// </summary>
+    public interface ITTS
+    {
+        /// <summary>
+        /// Speak
+        /// </summary>
+        /// <param name="textToSpeak">
+        /// 読上げるテキスト</param>
+        void Speak(string textToSpeak);
+    }
+}
---
+++ ACT.TTSInterface/ACT.TTSInterface/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ACT.TTSInterface")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("anoyetta")]
+[assembly: AssemblyProduct("ACT.TTSInterface")]
+[assembly: AssemblyCopyright("Copyright (c) 2014, anoyetta")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("a976e428-0eaa-48af-b7f8-2f8001b05ec3")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
